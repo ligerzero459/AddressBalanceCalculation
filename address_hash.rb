1e75b05522a438dde0761d857c37de8b929dccf2
Ryan Mottley <ligerzero459@gmail.com>
2015-12-30 14:33:44 -0600

    combine inputs or outputs from the same transaction into one entry

--- address_hash.rb
+++ address_hash.rb
@@ -9,7 +9,7 @@ require './models/transaction'
 require './models/input'
 require './models/output'
 
-address = ''
+address = 'PRQBLgPak9i2xWsdFWSMQfWEA8yWqieTKo'
 transactions = []
 balance = 0.0
 
@@ -17,29 +17,54 @@ outputs = Output.where(:address => address)
 inputs = Input.where(:address => address)
 
 outputs.each do |output|
-  output_hash = {
-      transaction_id: output.transaction_id,
-      txid: Transaction.where(:id=>output.transaction_id).get(:txid),
-      address: output.address,
-      value: output.value.round(6),
-      type: 'output',
-      n: output.n,
-      balance: 0.0
-  }
-  transactions.push output_hash
+  if transactions.last != nil
+    if transactions.last[:transaction_id] == output.transaction_id
+      transactions.last[:value] += output.value.round(6)
+    else
+      output_hash = {
+          transaction_id: output.transaction_id,
+          txid: Transaction.where(:id=>output.transaction_id).get(:txid),
+          address: output.address,
+          value: output.value.round(6),
+          type: 'output',
+          n: output.n,
+          balance: 0.0
+      }
+
+      transactions.push output_hash
+    end
+  else
+    output_hash = {
+        transaction_id: output.transaction_id,
+        txid: Transaction.where(:id=>output.transaction_id).get(:txid),
+        address: output.address,
+        value: output.value.round(6),
+        type: 'output',
+        n: output.n,
+        balance: 0.0
+    }
+
+    transactions.push output_hash
+  end
 end
 
 inputs.each do |input|
-  input_hash = {
-      transaction_id: input.transaction_id,
-      txid: Transaction.where(:id=>input.transaction_id).get(:txid),
-      address: input.address,
-      value: -input.value.round(6),
-      type: 'input',
-      n: input.vout,
-      balance: 0.0
-  }
-  transactions.push input_hash
+  if transactions.last[:transaction_id] == input.transaction_id
+    transactions.last[:value] += -input.value.round(6)
+  else
+    input_hash = {
+        transaction_id: input.transaction_id,
+        txid: Transaction.where(:id=>input.transaction_id).get(:txid),
+        address: input.address,
+        value: -input.value.round(6),
+        type: 'input',
+        n: input.vout,
+        balance: 0.0
+    }
+
+    transactions.push input_hash
+  end
+
 end
 
 transactions.sort_by! { |hsh| [hsh[:transaction_id], hsh[:n]] }
